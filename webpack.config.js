6c118eaee49df041c54331ef6963000e0190e1c7
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2021-04-05 17:32:50 +0200

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,26 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = {
-
-    mode: "production",
-
+    mode: 'production',
     entry: {
-        "event-delegation": "./src/index.ts",
-        "event-delegation.min": "./src/index.ts",
+        'event-delegation': './src/index.ts',
+        'event-delegation.min': './src/index.ts',
     },
-
     output: {
         path: resolve(__dirname, 'umd'),
         filename: '[name].js',
         library: 'EventDelegation',
-        libraryExport: 'default',
         libraryTarget: 'umd',
     },
-
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                test: /\.min\.js$/,
-                sourceMap: true,
-            }),
-        ],
-    },
-
-    devtool: "source-map",
+    devtool: 'source-map',
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: ['.ts', '.js']
     },
-
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                loaders: ["ts-loader"],
+                use: 'ts-loader',
                 exclude: /node_modules/,
             }
         ],
