bf6393a1349cea4bb7289757a130b240a5b6bd67
Jeffrey Westerkamp <jeffrey@pxlwidgets.com>
2021-04-07 22:39:05 +0200

    Added test for createBuilder() & mocked createBuilder() for index test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,15 @@
 import EventDelegation from '../src'
 import { EventHandler as _EventHandler } from '../src/EventHandler'
 import { normalizeRoot as _normalizeRoot } from '../src/lib/normalizeRoot'
+import { createBuilder as _createBuilder } from '../src/lib/createBuilder'
 
-const EventHandler = _EventHandler as jest.Mock
+const createBuilder = _createBuilder as jest.Mock
 const normalizeRoot = _normalizeRoot as jest.Mock
+const EventHandler = _EventHandler as jest.Mock
 
-jest.mock('../src/EventHandler', () => ({
-    EventHandler: jest.fn()
-}))
+jest.mock('../src/EventHandler', () => ({ EventHandler: jest.fn() }))
+jest.mock('../src/lib/createBuilder', () => ({ createBuilder: jest.fn() }))
+
 
 let fetchedRoot: HTMLElement
 
@@ -18,80 +20,34 @@ jest.mock('../src/lib/normalizeRoot', () => ({
 describe('index', () => {
 
     beforeEach(() => {
-        EventHandler.mockClear()
+        createBuilder.mockClear()
         normalizeRoot.mockClear()
     })
 
     describe('EventDelegation.global()', () => {
 
-        test('Passes all option values unaltered to the EventHandler constructor', () => {
-            const eventType = 'focus'
-            const selector = 'input'
-            const listener = () => undefined
-            const listenerOptions = {}
+        test('Calls createBuilder() with document.body', () => {
+            EventDelegation.global()
 
-            EventDelegation
-                .global()
-                .events(eventType)
-                .select(selector)
-                .listen(listener, listenerOptions)
-
-            expect(EventHandler).toBeCalledTimes(1)
-            expect(EventHandler).toBeCalledWith(expect.objectContaining({
-                eventType,
-                selector,
-                listener,
-                listenerOptions
-            }))
-        })
-
-        test('Uses document.body as the root', () => {
-            EventDelegation.global().events('doesnt').select('matter').listen(() => undefined)
-            expect(EventHandler).toBeCalledWith(expect.objectContaining({ root: document.body }))
+            expect(createBuilder).toBeCalledTimes(1)
+            expect(createBuilder).toBeCalledWith(document.body)
         })
     })
 
     describe('EventDelegation.within()', () => {
-        test('Passes all non-root option values unaltered to the EventHandler constructor', () => {
-            const eventType = 'click'
-            const selector = 'button'
-            const listener = () => undefined
-            const listenerOptions = {}
-
-            EventDelegation
-                .within('')
-                .events(eventType)
-                .select(selector)
-                .listen(listener, listenerOptions)
-
-            expect(EventHandler).toBeCalledTimes(1)
-            expect(EventHandler).toBeCalledWith(expect.objectContaining({
-                eventType,
-                selector,
-                listener,
-                listenerOptions
-            }))
-        })
 
         test('Finds the first root element when a selector is given for root', () => {
-            EventDelegation
-                .within('.my-other-root')
-                .events('dont')
-                .select('matter')
-                .listen(() => undefined)
-
+            EventDelegation.within('.my-other-root')
 
             expect(normalizeRoot).toBeCalledTimes(1)
             expect(normalizeRoot).toBeCalledWith('.my-other-root')
-            expect(EventHandler).toBeCalledWith(expect.objectContaining({
-                root: fetchedRoot,
-            }))
+            expect(createBuilder).toBeCalledWith(fetchedRoot)
         })
 
         test('Uses a given root when it is an element', () => {
             const root = document.createElement('div')
-            EventDelegation.within(root).events('doesnt').select('matter').listen(() => undefined)
-            expect(EventHandler).toBeCalledWith(expect.objectContaining({ root }))
+            EventDelegation.within(root)
+            expect(createBuilder).toBeCalledWith(root)
         })
     })
 
---
+++ test/lib/createBuilder.test.ts
@@ -0,0 +1,32 @@
+import { EventHandler as _EventHandler } from '../../src/EventHandler'
+import { createBuilder } from '../../src/lib/createBuilder'
+
+const EventHandler = _EventHandler as jest.Mock
+
+jest.mock('../../src/EventHandler', () => ({
+    EventHandler: jest.fn()
+}))
+
+describe('createBuilder()', () => {
+    test('Passes all option values unaltered to the EventHandler constructor', () => {
+        const root = document.createElement('div')
+        const eventType = 'focus'
+        const selector = 'input'
+        const listener = () => undefined
+        const listenerOptions = {}
+
+        createBuilder(root)
+            .events(eventType)
+            .select(selector)
+            .listen(listener, listenerOptions)
+
+        expect(EventHandler).toBeCalledTimes(1)
+        expect(EventHandler).toBeCalledWith(expect.objectContaining({
+            root,
+            eventType,
+            selector,
+            listener,
+            listenerOptions
+        }))
+    })
+})
