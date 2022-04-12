f63f4443aa068c88f7896f465ab2aee9662e65e9
Jeffrey Westerkamp <jeffreywesterkamp@hotmail.com>
2022-04-12 13:48:25 +0200

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
-    target: 'es5',
+    target: ['web', 'es5'],
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin({
